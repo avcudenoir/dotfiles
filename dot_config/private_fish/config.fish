30de03adb2f0c504de31f537cfa55a23567adde9
Alan Matuszczak <contact@alanmatuszczak.me>
2024-12-08 17:51:03 +0100

    converted fish config from zsh

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,29 @@
+if status is-interactive
+
+    [ -f ~/.shell_extras_fish.sh ] && source ~/.shell_extras_fish.sh
+
+    fzf --fish | source
+    atuin init fish | source
+    starship init fish | source
+    mise activate fish | source
+    direnv hook fish | source
+    eval (/opt/homebrew/bin/brew shellenv)
+
+    set -gx EDITOR 'zed --wait'
+    set -gx GOPATH "$HOME/go"
+    set -gx GOBIN "$HOME/go/bin"
+    set -gx GEM_HOME "$HOME/.gems"
+    set -gx CARGO_BIN "$HOME/.cargo/bin"
+
+    set -gx AWS_PAGER ""
+
+    set -gx TF_PLUGIN_CACHE_DIR "$HOME/.terraform.d/plugin-cache"
+    set -gx TF_PLUGIN_CACHE_MAY_BREAK_DEPENDENCY_LOCK_FILE 1
+    set -gx TERRAGRUNT_TFPATH (mise which ~/.local/share/mise/shims/terraform)
+    set -gx LC_ALL "pl_PL.UTF-8"
+    set -gx LANG "pl_PL.UTF-8"
+
+    set -gx GPG_TTY (tty)
+
+    fish_add_path $GEM_HOME $GOBIN $CARGO_BIN
+end
---
+++ dot_shell_extras_fish.sh
@@ -0,0 +1,76 @@
+# Define aliases
+alias mv='mv -i'
+alias cp='cp -i'
+alias ln='ln -i'
+alias suspend='systemctl suspend'
+alias df='df -H'
+alias du='du -H'
+alias genpasswd="strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n'; echo"
+alias meminfo='free -m -l -t'
+alias countFiles='find . -type f | wc -l'
+alias removeOldContainers='docker rm (docker ps -q -f status=exited)'
+alias removeOldImages='docker rmi (docker images --filter "dangling=true" -q --no-trunc)'
+alias gbr="git branch | grep -v \"master\" | xargs git branch -D"
+alias c_dryrun="chezmoi git pull -- --autostash --rebase && chezmoi diff"
+
+# Define functions
+function up
+    set DEEP $argv[1]
+    if test -z "$DEEP"
+        set DEEP 1
+    end
+    for i in (seq 1 $DEEP)
+        cd ..
+    end
+end
+
+function convertToUTF8
+    # $argv[1] - input format; $argv[2] - input file; $argv[3] - output file
+    iconv -f $argv[1] -t utf8 $argv[2] > $argv[3]
+end
+
+# Git related aliases
+alias gits='git status'
+alias gita='git add'
+alias gitb='git branch'
+alias gitc='git commit'
+alias gitd='git diff'
+alias gitgo='git checkout'
+
+function fd
+    cd (find $HOME -type d | fzf -i)
+end
+
+# cdf - cd into the directory of the selected file
+function cdf
+    set file (fzf +m -q $argv[1])
+    set dir (dirname $file)
+    cd $dir
+end
+
+function fe
+    set files
+    while read -l line
+        set files $files $line
+    end < (fzf-tmux --query=$argv[1] --multi --select-1 --exit-0)
+    if test -n "$files"
+        $EDITOR $files
+    end
+end
+
+# fkill - kill process
+function fkill
+    set pid (ps -ef | sed 1d | fzf -m | awk '{print $2}')
+    if test -n "$pid"
+        echo $pid | xargs kill -$argv[1]
+    end
+end
+
+# fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+function fbr
+    set branches branch
+    git fetch
+    set branches (git branch -a)
+    set branch (echo $branches | fzf-tmux +m)
+    git checkout (echo $branch | sed "s/.* //" | sed 's#remotes/[^/]*/##')
+end
